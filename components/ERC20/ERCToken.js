1f19989954e27f8e2c779262f45da20f86ca1ed7
Lp700s <siddhant@studio137.co.za>
2022-08-06 18:04:39 +0530

    send transaction

---
+++ components/ERC20/ERCToken.js
@@ -0,0 +1,2 @@
+module.exports = require("../solidity/artifacts/src/solidity/contracts/ERC20Token.sol/ERC20Token.json");
+// module.exports = require("");
---
+++ utils/EthereumApi.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+import Web3 from "web3";
+
+let web3;
+
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  // we are in the browser and metamask is running
+  window.ethereum.request({ method: "eth_requestAccounts" });
+  web3 = new Web3(window.ethereum);
+} else {
+  // we are on the server *OR* the user is not running metamask
+  // https://medium.com/jelly-market/how-to-get-infura-api-key-e7d552dd396f
+  const provider = new Web3.providers.HttpProvider(
+    "https://goerli.infura.io/v3/14a92a90d93b4c9b96f06e39382dad69"
+  );
+  web3 = new Web3(provider);
+}
+
+// const Web3 = require("web3");
+
+// const web3 = new Web3(window.ethereum);
+
+const Decimal = require("decimal.js");
+const applyDecimals = (rawValue, decimals, sign = "negative") => {
+  if (!rawValue) return "";
+
+  return Decimal(rawValue)
+    .mul(Decimal(10).pow(Decimal(sign === "positive" ? decimals : -decimals)))
+    .toFixed();
+};
+
+module.exports = {
+  applyDecimals,
+  web3,
+};
